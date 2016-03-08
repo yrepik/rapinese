ce2c0e4acb1a85fe1455753dbede517bd0a883c4
Medaglia, Germán <german.medaglia@gmail.com>
2016-03-08 10:14:42 -0300

    Upgrade to Laravel 5.2.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,10 +2,10 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Routes File
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you will register all of the routes in an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
@@ -15,6 +15,17 @@ Route::get('/', function()
 {
 	return view('home');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
 
 Route::group(['middleware' => ['web']], function() {  
     Route::get('/products', ['as' => 'products', 'uses' => 'ProductsController@getIndex']);
