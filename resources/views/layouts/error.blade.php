d417f44004993d7a8a533930f6730e301feec4e0
Medaglia, Germán <german.medaglia@gmail.com>
2016-06-24 01:33:56 -0300

    Web font updates.

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<div class="row">
 		<div class="col-md-3 text-center">
-            <span class="car-icon car-icon-car-insurance" style="font-size: 260px;"></span>
+            <span class="rapinese-icon rapinese-icon-car-insurance" style="font-size: 260px;"></span>
 		</div>
 		<div class="col-md-9">
     		<h1>@yield('message')</h1>
--- resources/views/under_construction.blade.php
+++ resources/views/under_construction.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<div class="row">
 		<div class="col-md-3 text-center">
-			<span class="car-icon car-icon-icon" style="font-size: 260px;"></span>
+			<span class="rapinese-icon rapinese-icon-icon" style="font-size: 260px;"></span>
 		</div>
 		<div class="col-md-9">
     		<h1>@lang('La página solicitada se encuentra en construcción.')</h1>
