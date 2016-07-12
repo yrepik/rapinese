773ada090bdd35cc138faa31c4a4fa87e604d8ad
Medaglia, Germán <german.medaglia@gmail.com>
2016-07-12 17:37:05 -0300

    503 view + labels.

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,39 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="{{ App::getLocale() }}">
     <head>
-        <title>Be right back.</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>RAPINESE @yield('title')</title>        
+        @section('styles')
+            {!! HTML::style('css/lib.css') !!}
+            {!! HTML::style('css/styles.css') !!}
+        @stop
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Oswald:300,400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+        @yield('styles')    
     </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
+    <body>       
+
+        <div id="page" class="container">        
+            
+            <div id="main" role="main" style="padding: 50px;">
+                <div class="row">
+                    <div class="col-md-3 text-center">
+                        <span class="rapinese-icon rapinese-icon-icon" style="font-size: 260px;"></span>
+                    </div>
+                    <div class="col-md-9 text-center">                        
+                        <h4 style="margin-bottom: 40px;">@lang('Estamos realizando tareas de mantenimiento. Pronto estaremos de regreso. Disculpe las molestias.')</h4>
+                        <ul class="list-inline">
+                            <li><small><span class="glyphicon glyphicon glyphicon-copyright-mark"></span> {{ config('app.company_name') }} - 1965-{{ date('Y') }}</small></li>
+                            <li><small><span class="glyphicon glyphicon-envelope ml20"></span> {{ config('app.contact_email') }}</small></li>
+                            <li><small><span class="glyphicon glyphicon-earphone ml20"></span> {{ config('app.contact_phones') }}</small></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
+            
         </div>
+       
     </body>
 </html>
